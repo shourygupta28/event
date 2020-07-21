5666f1e209af73706ee6cb704fc65fe7945a23e0
N15H4N7 <ngupta_be19@thapar.edu>
2020-07-21 15:41:04 +0530

    Trading

--- home/views.py
+++ home/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 def home(request):
 	return render(request, 'home/index.html', )
 
+def trading(request):
+	return render(request, 'home/trading.html')
 # Create your views here.
