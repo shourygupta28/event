df3b7e83e675763dfc1f29af225588088cce7ffe
N15H4N7 <ngupta_be19@thapar.edu>
2020-07-25 17:53:39 +0530

    Message

--- users/views.py
+++ users/views.py
@@ -2,12 +2,16 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from .forms import UserRegistrationForm
 from django.contrib.auth import get_user_model
+from django.contrib import messages
+
 
 def register(request):
     if request.method == 'POST':
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
             User = form.save()
+
+            messages.success(request, f'Your account has been created! You can login now.')
 
     else:
         form = UserRegistrationForm()
