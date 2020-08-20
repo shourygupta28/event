814338ccafa67df710fe96d91e3f1a3d61413838
tanya1810 <tanya.sood.311@gmail.com>
2020-08-20 12:22:29 +0530

    changes

--- home/models.py
+++ home/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from users.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class Company(models.Model):
@@ -12,8 +13,8 @@ class Company(models.Model):
 
 class Trading(models.Model):
     company = models.ForeignKey(Company, related_name = 'company_details_trading', on_delete=models.CASCADE, null=True, blank=True)
-    your_bid_price = models.IntegerField(default=0)
-    highest_bid = models.IntegerField(default=0)
+    your_bid_price = models.PositiveIntegerField(default=0)
+    highest_bid = models.PositiveIntegerField(default=0)
     buyer = models.ForeignKey(User, related_name = 'trading_buyer', on_delete=models.CASCADE, null=True, blank=True)
     seller = models.ForeignKey(User, related_name = 'trading_seller', on_delete=models.CASCADE,  null=True, blank=True)
     percentage_for_sale = models.DecimalField(default=1, decimal_places = 0, max_digits = 3 )
@@ -24,14 +25,14 @@ class Trading(models.Model):
 class Share(models.Model):
     company = models.ForeignKey(Company, default=None, on_delete=models.CASCADE ,related_name = 'company_details')
     shareholder = models.ForeignKey(User, default=None, on_delete=models.CASCADE, related_name = 'shareholder_details')
-    percentage_of_share = models.IntegerField() 
+    percentage_of_share = models.PositiveIntegerField(validators=[MaxValueValidator(100)]) 
     
     def __str__(self):
         return self.company.company_name + " - " + str(self.percentage_of_share)
 
 class Bidding(models.Model):
     company = models.ForeignKey(Company, related_name = 'company_details_bidding', on_delete=models.CASCADE)
-    bidding_price = models.IntegerField()
+    bidding_price = models.PositiveIntegerField()
     buyer = models.ForeignKey(User, related_name = 'bidding_buyer', on_delete=models.CASCADE, null=True, blank=True)
     visible = models.BooleanField(default = False)
 
